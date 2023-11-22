262882713eab27f03ce0d6f08541a30cf73a7211
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-22 09:16:47 +0900

    App 파일정리 및 CSS 정리

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <nav className="navbar">NavBar content</nav>
+        <main>Main Content</main>
+      </div>
+    </>
+  );
+}
+
+export default App;
