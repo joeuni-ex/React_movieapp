ccf4e022898746345dcd236e8d9855506f9ddde9
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-22 15:57:09 +0900

    테마 설정 추가(다크모드,라이트모드)

--- src/components Navbar/Navbar/Navbar.jsx
+++ src/components Navbar/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import Fire from "../../assets/fire.png";
 import Star from "../../assets/glowing-star.png";
 import Party from "../../assets/partying-face.png";
+import DarkMode from "../DarkMode/DarkMode";
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <h1>MovieApp</h1>
-
       <div className="navbar_links">
+        <DarkMode />
         <a href="">
           인기순
           <img className="navbar_emoji" src={Fire} alt="fire emoji" />
